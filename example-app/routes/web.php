5f797ff4c09f377bb557fb2bbd9b5fd453261385
Anthony Sibaja <sibajagranados@gmail.com>
2024-01-23 11:38:29 -0600

    dia 2 p1

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 //});
 
 use App\Http\Controllers\PostController; //
+
+
+
 //////////Llamar metodos de controller a router////////////////
 
 //Route::get('/post/{id}', [PostController::class, 'index']);
@@ -25,8 +28,8 @@ use App\Http\Controllers\PostController; //
 
 
 /////////////Trabajando con view////////////////
-Route::get('/contact', [PostController::class, 'contact']);
-Route::get('post/{id}/{name}/{password}', 'App\Http\Controllers\PostController@show_post');
+//Route::get('/contact', [PostController::class, 'contact']);
+//Route::get('post/{id}/{name}/{password}', 'App\Http\Controllers\PostController@show_post');
 
 //Practica de router como poner direcciones como tambien 
 //pasar parametros y url en la que estamos
@@ -48,3 +51,53 @@ Route::get('post/{id}/{name}/{password}', 'App\Http\Controllers\PostController@s
     //$url = route('admin.home');
     //return "this url is ". $url;
 //}));
+
+////base de datos ////
+
+///////////Insertar datos/////////////////////
+//Route::get('/insert', function(){
+  //DB::insert('insert into post(titulo, body) values(?, ?)', ['PHP con laravel', 'Laravel es muy completo y está bien tuanis para aprenderlo']);
+
+//});
+
+///////////leer datos/////////////////////
+
+// Route::get('/read', function(){
+//   $resultado = DB::select('select * from post where id = ?', [1]);
+//   return $resultado;
+//   //foreach($resultado as $post){
+//   //  return $post->titulo; 
+//   //}
+
+// });
+
+//////////actualizar datos///////////
+
+// Route::get('/update',function(){
+//     $updated = DB::update('update post set titulo= "Peppito clavo un clavito"where id=?',[2]);
+
+// });
+
+///////////eliminar datos//////////////
+
+// Route::get('/eliminar', function(){
+
+//     $eliminar = DB::delete('delete from post where id = ?', [1]);
+//     return $eliminar;
+
+// });
+
+
+////////////elocuencia//////////
+
+use App\Models\Post;
+
+Route::get('/find', function(){
+    $post = Post::all(); 
+    
+
+    foreach($post as $post) {
+       return $post->titulo;
+    }
+
+});
